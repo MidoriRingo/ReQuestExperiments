ca8415551bf36b9419d841203782914d272eed3f
maxml <maxml@inbox.ru>
2014-04-23 15:03:24 +0300

    google+ v0.1

--- parse-js-blank/js/registration.js
+++ parse-js-blank/js/registration.js
@@ -4,87 +4,69 @@
  * and open the template in the editor.
  */
 
-var OAUTHURL = 'https://accounts.google.com/o/oauth2/auth?';
-var VALIDURL = 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=';
-var SCOPE = 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email';
-var CLIENTID = '716569014051.apps.googleusercontent.com';
-var REDIRECT = 'http://www.gethugames.in/proto/googleapi/oauth/'
-var LOGOUT = 'http://accounts.google.com/Logout';
-var TYPE = 'token';
-var _url = OAUTHURL + 'scope=' + SCOPE + '&client_id=' + CLIENTID + '&redirect_uri=' + REDIRECT + '&response_type=' + TYPE;
-var acToken;
-var tokenType;
-var expiresIn;
-var user;
-var loggedIn = false;
+function signinCallback(authResult) {
 
-function login() {
-    var win = window.open(_url, "windowname1", 'width=800, height=600');
+    if (authResult['access_token']) {
 
-    var pollTimer = window.setInterval(function() {
-        try {
-            console.log(win.document.URL);
-            if (win.document.URL.indexOf(REDIRECT) != -1) {
-                window.clearInterval(pollTimer);
-                var url = win.document.URL;
-                acToken = gup(url, 'access_token');
-                tokenType = gup(url, 'token_type');
-                expiresIn = gup(url, 'expires_in');
-                win.close();
+        gapi.auth.setToken(authResult);
+        gapi.client.load('plus', 'v1', function() {
+            getName();
+        });
 
-                validateToken(acToken);
-            }
-        } catch (e) {
+        getEmail();
+        $('#revokeButton').show().data('loggedInWith', 'g+').text('Sign Out');
+        $('#signinButton').css('display', 'none');
+    
+    } else if (authResult[ 'error' ]) {
+        console.log('Sign-in state: ' + authResult[ 'error' ]);
+    }
+}
+
+function disconnectUser(access_token) {
+    var revokeUrl = 'https://accounts.google.com/o/oauth2/revoke?token=' + access_token;
+
+    $.ajax({
+        type: 'GET',
+        url: revokeUrl,
+        async: false,
+        contentType: "application/json",
+        dataType: 'jsonp',
+        success: function(nullResponse) {
+            $('#revokeButton').hide();
+            $('#signinButton').show();
+            alert('This app has had its G+ access revoked');
+        },
+        error: function(e) {
+            console.log(e);
         }
-    }, 500);
-}
-
-function validateToken(token) {
-    $.ajax({
-        url: VALIDURL + token,
-        data: null,
-        success: function(responseText) {
-            getUserInfo();
-            loggedIn = true;
-            $('#loginText').hide();
-            $('#logoutText').show();
-        },
-        dataType: "jsonp"
     });
 }
 
-function getUserInfo() {
-    $.ajax({
-        url: 'https://www.googleapis.com/oauth2/v1/userinfo?access_token=' + acToken,
-        data: null,
-        success: function(resp) {
-            user = resp;
-            console.log(user);
-            $('#uName').text('Welcome ' + user.name);
-            $('#imgHolder').attr('src', user.picture);
-        },
-        dataType: "jsonp"
+function getName() {
+
+    var request = gapi.client.plus.people.get({'userId': 'me'});
+    request.execute(function(resp) {
+        var username = String(resp.displayName).split(' ');
+        $('#name').text('User: ' + username[0] + ' ' + username[1]);
     });
 }
 
-//credits: http://www.netlobo.com/url_query_string_javascript.html
-function gup(url, name) {
-    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-    var regexS = "[\\#&]" + name + "=([^&#]*)";
-    var regex = new RegExp(regexS);
-    var results = regex.exec(url);
-    if (results == null)
-        return "";
-    else
-        return results[1];
+function getEmail() {
+    gapi.client.load('oauth2', 'v2', function() {
+        var request = gapi.client.oauth2.userinfo.get();
+        request.execute(function(resp) {
+            if (resp[ 'email' ]) {
+                $('#email').text('Email ' + resp[ 'email' ]);
+            }
+        });
+    });
 }
 
-function startLogoutPolling() {
-    $('#loginText').show();
-    $('#logoutText').hide();
-    loggedIn = false;
-    $('#uName').text('Welcome ');
-    $('#imgHolder').attr('src', 'none.jpg');
-}
+$('#revokeButton').click(function() {
+    if ($(this).data('loggedInWith') === 'g+') {
+        disconnectUser(gapi.auth.getToken().access_token);
+    }
 
-
+    $('#name').text('Name: ');
+    $('#email').text('Email: ');
+});
