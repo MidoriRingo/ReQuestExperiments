81ff0fa46ad3902f2dd16407a9d9b61396b6f145
maxml <maxml@inbox.ru>
2014-04-26 07:17:34 +0300

    worked details, not worked respond

--- parse-js-blank/js/details.js
+++ parse-js-blank/js/details.js
@@ -3,15 +3,52 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$(document).ready(function() {
 
-    $('#main').on('click', 'vacancy', function(event) {
-        var vacancy = $(event.delegateTarget);
-        console.log(vacancy);
-//    vacancy.find("hidden")
+var vacancy;
+function linkClicked(id) {
+
+    var Req = Parse.Object.extend("Requests");
+
+    var query = new Parse.Query(Req);
+    query.equalTo("objectId", id);
+
+    query.find({
+        success: function(results) {
+            // Do something with the returned Parse.Object values
+            var object = results[0];
+            vacancy = object;
+            console.log(vacancy);
+
+            showDetails();
+        },
+        error: function(error) {
+            console.log("Error: " + error.code + " " + error.message);
+        }
     });
-});
+}
 
-function linkClicked(id){
-    alert("Id="+id);
+function showDetails() {
+    $('#main').empty();
+    $('#main').load('form/details.html #details_form > *');
+
+    console.log(vacancy.get("expire"));
+    setTimeout(function() {
+        $("#vName").html(vacancy.get("title"));
+        $("#vExpireDate").html(String(vacancy.get("expire")).substring(0, 24));
+        $("#vReward").html(vacancy.get("reward"));
+        $("#vCompanyName").html(vacancy.get("company"));
+        $("#vDesc").html(vacancy.get("company_description"));
+        $("#vDemands").html(vacancy.get("demands"));
+        $("#vTerms").html(vacancy.get("terms"));
+        $("#vSalary").html(vacancy.get("salary"));
+        $("#vCity").html(vacancy.get("city"));
+        $("#vAdress").html(vacancy.get("company_address"));
+    }, 1500);
+
+}
+
+function getRespondForm(){
+    $('#main').empty();
+    $('#main').load('form/new_respond.html #respond_form > *');
+
 }
